3b4384e14a077c08c9d2d76388740ba9ace9944b
Linging <lingbunny@icloud.com>
2016-03-30 11:14:41 +0800

    finish employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,25 @@
 class EmployeesController < ApplicationController
+	def login
+	end
+
+	def create
+		@employee = Employee.new(employee_param)
+		@employee.save
+		respond_to do |format|
+			format.json{ render :json => @employee }
+		end
+		redirect_to usermac_portal_path
+	end
+
+	def show
+	end
+
+	def index
+		@employees = Employee.all
+	end
+
+	private
+	  def  employee_param
+	      params.permit(:name, :mac)
+	  end
 end
